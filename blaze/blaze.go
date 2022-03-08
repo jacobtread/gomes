ff91b45e028437381930737408de249c1aac2bc5
Jacobtread <jacobtread@gmail.com>
2022-03-08 16:17:04 +1300

    Read counter and read all packets

--- blaze/blaze.go
+++ blaze/blaze.go
@@ -2,6 +2,7 @@ package blaze
 
 import (
 	"bufio"
+	"container/list"
 	"encoding/binary"
 	"io"
 	"net"
@@ -29,27 +30,34 @@ type Packet struct {
 	Content   []byte
 }
 
+type ReadCounter struct {
+	Count int
+}
+
 // UInt16 reads an uint16 from the provided packet buffer using the
 // big endian byte order
-func (b *PacketBuff) UInt16() uint16 {
+func (b *PacketBuff) UInt16(c *ReadCounter) uint16 {
 	var out uint16
 	_ = binary.Read(b, binary.BigEndian, &out)
+	c.Count += 2
 	return out
 }
 
 // UInt32 reads an uint32 from the provided packet buffer using the
 // big endian byte order
-func (b *PacketBuff) UInt32() uint32 {
+func (b *PacketBuff) UInt32(c *ReadCounter) uint32 {
 	var out uint32
 	_ = binary.Read(b, binary.BigEndian, &out)
+	c.Count += 4
 	return out
 }
 
 // Float64 reads a float64 from the provided packet buffer using the
 // big endian byte order
-func (b *PacketBuff) Float64() float64 {
+func (b *PacketBuff) Float64(c *ReadCounter) float64 {
 	var out float64
 	_ = binary.Read(b, binary.BigEndian, &out)
+	c.Count += 8
 	return out
 }
 
@@ -69,9 +77,25 @@ func (b *PacketBuff) WriteVarInt(value int64) {
 }
 
 // ReadVarInt reads a var int from the packet buffer
-func (b *PacketBuff) ReadVarInt() uint64 {
-	out, _ := binary.ReadUvarint(b)
-	return out
+func (b *PacketBuff) ReadVarInt(c *ReadCounter) uint64 {
+	var x uint64
+	var s uint
+	for i := 0; i < 10; i++ {
+		b, err := b.ReadByte()
+		c.Count++
+		if err != nil {
+			return x
+		}
+		if b < 0x80 {
+			if i == 9 && b > 1 {
+				return x
+			}
+			return x | uint64(b)<<s
+		}
+		x |= uint64(b&0x7f) << s
+		s += 7
+	}
+	return x
 }
 
 // WriteNum takes any number type and writes it to the packet
@@ -80,11 +104,12 @@ func (b *PacketBuff) WriteNum(value any) {
 }
 
 // ReadString reads a string from the buffer
-func (b *PacketBuff) ReadString() string {
-	l := b.ReadVarInt()
+func (b *PacketBuff) ReadString(c *ReadCounter) string {
+	l := b.ReadVarInt(c)
 	buf := make([]byte, l)
 	_, _ = io.ReadFull(b, buf)
 	_, _ = b.ReadByte() // Strings end with a zero byte
+	c.Count += len(buf) + 1
 	return string(buf)
 }
 
@@ -101,17 +126,22 @@ func (b *PacketBuff) WriteString(value string) {
 	_ = b.WriteByte(0)
 }
 
+var defaultCounter = &ReadCounter{}
+
 // ReadPacket reads a game packet from the provided packet reader
-func (b *PacketBuff) ReadPacket() *Packet {
+func (b *PacketBuff) ReadPacket(c *ReadCounter) *Packet {
+	if c == nil {
+		c = defaultCounter
+	}
 	packet := Packet{
-		Length:    b.UInt16(),
-		Component: b.UInt16(),
-		Command:   b.UInt16(),
-		Error:     b.UInt16(),
-		QType:     b.UInt16(),
+		Length:    b.UInt16(c),
+		Component: b.UInt16(c),
+		Command:   b.UInt16(c),
+		Error:     b.UInt16(c),
+		QType:     b.UInt16(c),
 	}
 	if (packet.QType * 0x10) != 0 {
-		packet.ExtLength = b.UInt16()
+		packet.ExtLength = b.UInt16(c)
 	} else {
 		packet.ExtLength = 0
 	}
@@ -122,6 +152,7 @@ func (b *PacketBuff) ReadPacket() *Packet {
 	if err != nil {
 		return nil
 	}
+	c.Count += len(bytes)
 	packet.Content = bytes
 	return &packet
 }
@@ -129,16 +160,19 @@ func (b *PacketBuff) ReadPacket() *Packet {
 // ReadPacketHeading reads a game packet from the provided packet reader.
 // but only reads the heading portion of the packet skips over the packet
 // contents.
-func (b *PacketBuff) ReadPacketHeading() *Packet {
+func (b *PacketBuff) ReadPacketHeading(c *ReadCounter) *Packet {
+	if c == nil {
+		c = defaultCounter
+	}
 	packet := Packet{
-		Length:    b.UInt16(),
-		Component: b.UInt16(),
-		Command:   b.UInt16(),
-		Error:     b.UInt16(),
-		QType:     b.UInt16(),
+		Length:    b.UInt16(c),
+		Component: b.UInt16(c),
+		Command:   b.UInt16(c),
+		Error:     b.UInt16(c),
+		QType:     b.UInt16(c),
 	}
 	if (packet.QType * 0x10) != 0 {
-		packet.ExtLength = b.UInt16()
+		packet.ExtLength = b.UInt16(c)
 	} else {
 		packet.ExtLength = 0
 	}
@@ -147,4 +181,14 @@ func (b *PacketBuff) ReadPacketHeading() *Packet {
 	bytes := make([]byte, l) // Create an empty byte array in place of the content
 	packet.Content = bytes
 	return &packet
+}
+
+func (b *PacketBuff) ReadAllPackets() *list.List {
+	out := list.New()
+	size := b.Reader.Size()
+	c := &ReadCounter{}
+	for c.Count < size {
+		out.PushBack(b.ReadPacket(c))
+	}
+	return out
 }
